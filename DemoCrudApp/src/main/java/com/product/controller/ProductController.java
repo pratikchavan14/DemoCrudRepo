22566041638fef064ecf5bcf4eb75c0f66c89ea3
ASUS <ASUS@192.168.1.22>
2022-04-21 14:39:10 +0530

    first method commit

--- DemoCrudApp/src/main/java/com/product/controller/ProductController.java
+++ DemoCrudApp/src/main/java/com/product/controller/ProductController.java
@@ -1,5 +1,13 @@
 package com.product.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
 	//this is first commit
+	
+	public String home() {
+		System.out.println("this my home url");
+		return "index";
+	}
 }
